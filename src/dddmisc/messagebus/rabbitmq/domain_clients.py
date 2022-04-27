d19b8e74e7074cd3e75a67bbad627736279ec3a9
Vladislav Vorobyov <vladislav.vorobyov@gmail.com>
2022-04-27 14:35:48 +0600

    refactoring rabbit domain clients

--- src/dddmisc/messagebus/rabbitmq/domain_clients.py
+++ src/dddmisc/messagebus/rabbitmq/domain_clients.py
@@ -1,100 +1,115 @@
 import typing as t
 
+import aio_pika
 from aio_pika.abc import AbstractExchange, AbstractQueue, AbstractRobustConnection, AbstractConnection, \
     AbstractIncomingMessage
+from yarl import URL
+
+from dddmisc.domain_message import get_message_class, Event
 from dddmisc.domain_message.messages import BaseMessage
 from dddmisc.messagebus.rabbitmq.abstract import AbstractRabbitDomainClient
 
 
 class RabbitSelfDomainClient(AbstractRabbitDomainClient):
-    async def handle(self, message: BaseMessage):
-        pass
 
     _event_exchange: AbstractExchange
     _response_exchange: AbstractExchange
     _command_queue: AbstractQueue
+    _command_consume_tag: str
 
-    def __init__(self, connection: AbstractRobustConnection,
-                 self_domain: str, connected_domain: str = None,
-                 permanent_consume=True, prefetch_count=0):
-        super(RabbitSelfDomainClient, self).__init__(connection, self_domain, self_domain, permanent_consume,
-                                                   prefetch_count)
+    def __init__(self, url: t.Union[str, URL], self_domain: str, connected_domain: str, *args, **kwargs):
+        super(RabbitSelfDomainClient, self).__init__(url, self_domain, self_domain, *args, **kwargs)
 
-    async def start(self):
-        self._event_exchange = await self._setup_event_publisher(self._connection)
-        self._response_exchange = await self._setup_response_publisher(self._connection)
-        await self._setup_command_consumer(self._connection, self.self_domain,
-                                           self._prefetch_count, self._permanent_consume)
-
-    async def stop(self, exception: Exception = None):
+    async def handle(self, message: BaseMessage):
         pass
 
-    @staticmethod
-    async def _setup_event_publisher(connection: AbstractRobustConnection) -> AbstractExchange:
-        event_publisher_channel = await connection.channel()
+    async def start(self):
+        await super(RabbitSelfDomainClient, self).start()
+        self._event_exchange = await self._setup_event_publisher()
+        self._response_exchange = await self._setup_response_publisher()
+        self._command_queue = await self._setup_command_consumer()
+        self._command_consume_tag = await self._command_queue.consume(self._command_callback)
+
+    async def stop(self, exception: BaseException = None):
+        await self._command_queue.cancel(self._command_consume_tag)
+        await super(RabbitSelfDomainClient, self).stop(exception)
+
+    async def _setup_event_publisher(self) -> AbstractExchange:
+        event_publisher_channel = await self._connection.channel()
         return await event_publisher_channel.declare_exchange('events', type='fanout', durable=True)
 
-    @staticmethod
-    async def _setup_response_publisher(connection: AbstractRobustConnection) -> AbstractExchange:
-        response_publisher_channel = await connection.channel()
+    async def _setup_response_publisher(self) -> AbstractExchange:
+        response_publisher_channel = await self._connection.channel()
         return response_publisher_channel.default_exchange
 
-    @staticmethod
-    async def _setup_command_consumer(connection: AbstractConnection, domain: str,
-                                      prefetch_count=0, permanent_consume=True) -> AbstractQueue:
-        command_consumer_channel = await connection.channel(publisher_confirms=False)
+    async def _setup_command_consumer(self) -> AbstractQueue:
+        command_consumer_channel = await self._connection.channel(publisher_confirms=False)
         await command_consumer_channel.declare_exchange('commands', type='fanout', durable=True)
-
-        await command_consumer_channel.set_qos(prefetch_count=prefetch_count)
+        await command_consumer_channel.set_qos(prefetch_count=self._prefetch_count)
         command_queue = await command_consumer_channel.declare_queue(
-            'commands', durable=permanent_consume, auto_delete=not permanent_consume)
-        await command_queue.bind('commands', f'{domain}.*')
+            'commands', durable=self._permanent_consume, auto_delete=not self._permanent_consume)
+        for command in self._commands:
+            await command_queue.bind('commands', f'{self.self_domain}.{command.__name__}')
         return command_queue
+
+    async def _command_callback(self, message: AbstractIncomingMessage):
+        publisher = message.user_id
+        routing_list = message.routing_key.split('.')
+        message_class = get_message_class(routing_list[0], routing_list[1])
+        domain_message = message_class.loads(message.body)
+        if isinstance(domain_message, Event):
+            await self._callback(domain_message, publisher)
+        else:
+            result = await self._callback(domain_message, publisher)
+            await self._publish_response(result, message.reply_to)
+
+    async def _publish_response(self, response, reply_to):
+        message = aio_pika.Message(response.dumps(), user_id=self.self_domain)
+        await self._response_exchange.publish(message, reply_to)
 
 
 class RabbitOtherDomainClient(AbstractRabbitDomainClient):
-    async def handle(self, message: BaseMessage):
-        pass
 
     _event_queue: AbstractQueue
     _command_exchange: AbstractExchange
     _response_queue: AbstractQueue
 
+    def __init__(self, *args, **kwargs):
+        super(RabbitOtherDomainClient, self).__init__(*args, ** kwargs)
+
     async def start(self):
-        self._event_queue = await self._setup_event_consumer(
-            self._connection, self.self_domain, [f'{self.connected_domain}.*'],
-            self._prefetch_count, self._permanent_consume)
-        self._command_exchange = await self._setup_command_publisher(self._connection)
-        self._response_queue = await self._setup_response_consumer(self._connection)
+        await super(RabbitOtherDomainClient, self).start()
+        self._event_queue = await self._setup_event_consumer()
+        self._command_exchange = await self._setup_command_publisher()
+        self._response_queue = await self._setup_response_consumer()
         await self._response_queue.consume(self._response_callback)
 
-    async def stop(self, exception: Exception = None):
-        pass
-
-    @staticmethod
-    async def _setup_event_consumer(connection: AbstractRobustConnection,
-                                    queue_name: str, binding_list: t.Iterable[str],
-                                    prefetch_count=0, permanent_consume=True) -> AbstractQueue:
-        event_consume_channel = await connection.channel()
+    async def _setup_event_consumer(self) -> AbstractQueue:
+        event_consume_channel = await self._connection.channel()
         await event_consume_channel.declare_exchange('events', type='fanout', durable=True)
-        await event_consume_channel.set_qos(prefetch_count=prefetch_count)
+        await event_consume_channel.set_qos(prefetch_count=self._prefetch_count)
         event_queue = await event_consume_channel.declare_queue(
-            queue_name, durable=permanent_consume, auto_delete=not permanent_consume)
-        for routing_key in binding_list:
+            self.self_domain, durable=self._permanent_consume, auto_delete=not self._permanent_consume)
+        for event in self._events:
+            routing_key = f'{self.connected_domain}.{event.__name__}'
             await event_queue.bind('events', routing_key)
         return event_queue
 
-    @staticmethod
-    async def _setup_command_publisher(connection: AbstractConnection) -> AbstractExchange:
-        command_publish_channel = await connection.channel()
+    async def _setup_command_publisher(self) -> AbstractExchange:
+        command_publish_channel = await self._connection.channel()
         command_exchange = await command_publish_channel.declare_exchange('commands', durable=True)
         return command_exchange
 
-    @staticmethod
-    async def _setup_response_consumer(connection: AbstractConnection) -> AbstractQueue:
-        response_consume_channel = await connection.channel()
+    async def _setup_response_consumer(self) -> AbstractQueue:
+        response_consume_channel = await self._connection.channel()
         response_queue = await response_consume_channel.declare_queue(exclusive=True, auto_delete=True)
         return response_queue
 
     async def _response_callback(self, message: AbstractIncomingMessage):
+        pass
+
+    async def _event_callback(self, message: AbstractIncomingMessage):
+        pass
+
+    async def handle(self, message: BaseMessage):
         pass
--- tests/integration/test_async_rabbit_domain_message_bus.py
+++ tests/integration/test_async_rabbit_domain_message_bus.py
@@ -1,6 +1,7 @@
 import asyncio
 from asyncio import AbstractEventLoop
 
+import aio_pika
 import pytest
 from aio_pika import RobustExchange, RobustQueue
 from aio_pika.abc import AbstractRobustExchange
@@ -15,39 +16,41 @@ class TestRabbitDomainClient:
 
     async def test_start_and_stop_self_domain_client(self, event_loop: AbstractEventLoop, rabbit):
         async with rabbit() as cfg:
+            client = RabbitSelfDomainClient(cfg.url, cfg.vhost, '', (), (), None)
 
-            mode = RabbitSelfDomainClient(cfg.vhost)
-            client = AsyncRabbitMessageBus(cfg.url, mode)
-            client.set_loop(event_loop)
             await client.start()
 
             assert client._connection.connected
-            assert mode._command_exchange.name == 'commands'
-            assert mode._event_exchange.name == 'events'
-            assert mode._command_queue.name == 'commands'
+            assert client._response_exchange.name == ''
+            assert client._event_exchange.name == 'events'
+            assert client._command_queue.name == 'commands'
 
             await client.stop()
-            connections = [conn for conn in await cfg.client.list_connections()
-                           if conn['vhost'] == cfg.vhost]
-            assert len(connections) == 0
 
     async def test_start_and_stop_other_domain_client(self, event_loop: AbstractEventLoop, rabbit):
         async with rabbit() as cfg:
             self_domain = random_word()
-            mode = RabbitOtherDomainClient(self_domain)
-            client = AsyncRabbitMessageBus(cfg.url, mode)
-            client.set_loop(event_loop)
+            client = RabbitOtherDomainClient(cfg.url, self_domain, cfg.vhost, (), (), None)
             await client.start()
 
             assert client._connection.connected
-            assert mode._event_queue.name == self_domain
-            assert mode._command_exchange.name == 'commands'
-            assert mode._response_queue.name != self_domain
+            assert client._event_queue.name == self_domain
+            assert client._command_exchange.name == 'commands'
+            assert client._response_queue.name != self_domain
 
             await client.stop()
-            connections = [conn for conn in await cfg.client.list_connections()
-                           if conn['vhost'] == cfg.vhost]
-            assert len(connections) == 0
+
+    # async def test_self_domain_command_consume(self, rabbit):
+    #     async with rabbit() as cfg:
+    #         client = RabbitSelfDomainClient(cfg.url, self_domain=cfg.vhost)
+    #
+    #         await client.start()
+    #
+    #
+    #
+    #         await client.stop()
+
+
 
     # async def test_publish_event(self, event_loop: AbstractEventLoop, rabbit):
     #
