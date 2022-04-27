007516e054ff860fb9cd398dc3b51a5da88b652d
Vladislav Vorobyov <vladislav.vorobyov@gmail.com>
2022-04-27 14:30:02 +0600

    Add methods to AbstractDomainMessage

--- src/dddmisc/domain_message/base.py
+++ src/dddmisc/domain_message/base.py
@@ -4,6 +4,9 @@ import typing as t
 from marshmallow import Schema
 
 
+T = t.TypeVar('T')
+
+
 class AbstractDomainMessage(abc.ABC):
     __schema__: Schema = Schema()
     __domain_name__: str
@@ -11,3 +14,23 @@ class AbstractDomainMessage(abc.ABC):
 
     def __init__(self, **kwargs):
         pass
+
+    @classmethod
+    @abc.abstractmethod
+    def load(cls: t.Type[T], data: dict) -> T:
+        ...
+
+    @classmethod
+    @abc.abstractmethod
+    def loads(cls: t.Type[T], data: bytes) -> T:
+        ...
+
+    @abc.abstractmethod
+    def dump(self) -> dict:
+        ...
+
+    @abc.abstractmethod
+    def dumps(self) -> bytes:
+        ...
+
+
